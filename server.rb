db1355ce2a5163b09f59edbe43dbf5a632b81b2b
Brian McClain <brianmmcclain@gmail.com>
2012-07-03 00:31:50 -0400

    Updated with temp landing page to test Discogs API

--- server.rb
+++ server.rb
@@ -2,6 +2,49 @@ require 'rubygems'
 require 'sinatra'
 require 'discogs'
 
+discogs = Discogs::Wrapper.new("JaaMbox")
+
 get "/" do
-  "Welcome to JaaMbox"
+  out =  "Welcome to JaaMbox<br /><br />"
+  out += "List all releases by an artist:"
+  out += "<form method='post' action='/artist'><br />"
+  out += "<input type='text' id='artist' name='artist' />"
+  out += "<input type='submit' />"
+  out += "</form>"
 end
+
+get "/artist/:artist" do
+  artist_name = params[:artist]
+  puts "Looking up #{artist_name}"
+  
+  begin
+    artist = discogs.get_artist(artist_name)
+    out = ""
+    artist.releases.each do |r|
+      out += r.title + "<br />"
+    end
+  
+    return out
+  rescue
+    return "No releases found"
+  end
+end
+
+post "/artist" do
+  artist_name = params[:artist]
+  puts "Looking up #{artist_name}"
+  
+  begin
+    artist = discogs.get_artist(artist_name)
+    out = ""
+    artist.releases.each do |r|
+      out += r.title + "<br />"
+    end
+
+    return out
+  rescue
+    return "No releases found"
+  end
+end
+  
+  
